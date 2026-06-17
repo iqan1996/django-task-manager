cf511ae4bd6e30616667e6e1fcfa54311f30ab91
Iqan <iqan@Iqan.localdomain>
2026-06-17 21:28:38 +0330

    Initial Django task manager project

---
+++ tasks/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "status", "created_at", "updated_at")
+    list_filter = ("status", "created_at", "updated_at")
+    search_fields = ("title", "description", "owner__username")
+    readonly_fields = ("created_at", "updated_at")
---
+++ tasks/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    owner_username = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Task
+        fields = [
+            'id',
+            'title',
+            'description',
+            'status',
+            'owner',
+            'owner_username',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = [
+            'id',
+            'owner',
+            'owner_username',
+            'created_at',
+            'updated_at',
+        ]
