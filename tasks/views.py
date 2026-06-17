c2aa3cf844bfb338781c709431107dbdc2e1cb4f
Iqan <iqan@Iqan.localdomain>
2026-06-17 22:34:47 +0330

    Permissions and views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+class TaskListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Task.objects.all().order_by('-id')
+
+        search_query = self.request.query_params.get('search')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query)
+            )
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TaskDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Task.objects.all()
