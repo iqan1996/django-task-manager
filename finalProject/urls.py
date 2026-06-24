28049c487ede0a0b3c429737e1b6b6e23d7d3348
Iqan <iqan@Iqan.localdomain>
2026-06-24 23:50:02 +0330

    Add root url

--- finalProject/urls.py
+++ finalProject/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView
 from tasks.forms import BootstrapAuthenticationForm
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
@@ -18,4 +20,5 @@ urlpatterns = [
     name="login",
 ),
 path("accounts/", include("django.contrib.auth.urls")),
+path("", RedirectView.as_view(pattern_name="tasks_web:task-list", permanent=False)),
 ]
